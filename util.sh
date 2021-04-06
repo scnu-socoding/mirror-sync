812c6b109b221894472b4d7cb71e48bd0460267b
Bobby Rong <rjl931189261@126.com>
2021-04-06 12:55:45 +0800

    another typo fix

--- util.sh
+++ util.sh
@@ -3,21 +3,22 @@
 rep(){
     i=1
     j=-5
+    rm -f /mnt/socoding/files/fancyindex/js/index.js.new
     while read line; do
         result=$(echo ${line} | grep "${1}")
         if [[ "$result" != "" ]]; then
             j=${i}
-            echo "$line"
+            echo "$line" >> /mnt/socoding/files/fancyindex/js/index.js.new
         elif ((i==${j}+1)); then
-            echo "text: 'stat: ${2}',"
+            echo "text: 'stat: ${2}'," >> /mnt/socoding/files/fancyindex/js/index.js.new
         elif ((i==${j}+2 && ${2}==0)); then
-            echo "color: 'mdl-chip mdl-color--green'"
-        elif ((i==${2}+2)); then
-            echo "color: 'mdl-chip mdl-color--red'"
+            echo "color: 'mdl-chip mdl-color--green'" >> /mnt/socoding/files/fancyindex/js/index.js.new
+        elif ((i==${j}+2)); then
+            echo "color: 'mdl-chip mdl-color--red'" >> /mnt/socoding/files/fancyindex/js/index.js.new
         else
-            echo "$line"
+            echo "$line" >> /mnt/socoding/files/fancyindex/js/index.js.new
         fi
         ((i++))
-    done < /mnt/socoding/files/fancyindex/js/index.js > /mnt/socoding/files/fancyindex/js/index.js.new
+    done < /mnt/socoding/files/fancyindex/js/index.js
     mv /mnt/socoding/files/fancyindex/js/index.js.new /mnt/socoding/files/fancyindex/js/index.js
 }
